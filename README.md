db09f750db8fa11725cdbb0f03ce22c857875aa5
loik <loikjoha@gmail.com>
2023-07-11 19:33:27 +0000

    ajout -b

--- README.md
+++ README.md
@@ -7,10 +7,16 @@ Pour compiler à la racine du fichier
 gcc ajc/exo11.c -o ajc/exo11.out
 ```
 
-pour lancer l'excuter
+pour lancer l'excuter sans option
 
 ```bash
-ajc/exo11.out ajc/file1.txt ajc/file1.txt 
+ajc/exo11.out ajc/file1.txt ajc/file2.txt ajc/merge.txt
 ```
 
-:-)
+pour lancer l'excuter avec option
+
+```bash
+ajc/exo11.out -b ajc/file1.txt ajc/file1.txt ajc/merge.bin
+```
+
+voilà
--- ajc/exo11.c
+++ ajc/exo11.c
@@ -5,10 +5,22 @@
 int main(int argc, char *argv[]){
     FILE* fptr = NULL;
     char c;
-    FILE* mergeptr = fopen("ajc/merge.txt", "w");
+    int start=1,end = argc-1;
+    char law1[3] = {'w'};
+    char law2[3] = {'r'};
 
-    for( int i = 1;i < argc;i++){
-        fptr = fopen(argv[i], "r");
+    if(argc == 5){
+        start = 2;
+        law1[1] = 'b';law1[2] = '\0';
+        law2[1] = 'b';law2[2] = '\0';
+    }
+
+
+
+    FILE* mergeptr = fopen(argv[end], law1);
+
+    for( int i = start;i < argc;i++){
+        fptr = fopen(argv[i], law2);
         
         while ( !((c = fgetc(fptr)) == EOF)){ 
             fprintf(mergeptr,"%c",c);
